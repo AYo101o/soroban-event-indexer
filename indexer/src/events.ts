402605f6585fef3dc62944738886c93ca367ba8d
AYo101o <ayoakinniyi10@gmail.com>
2026-08-23 09:21:37 -0700

    feat: add xdr event value decoding utility

--- indexer/src/events.ts
+++ indexer/src/events.ts
@@ -1,6 +1,14 @@
 import { server } from "./rpc";
 import { withRetry } from "./retry";
+import { scValToNative } from "@stellar/stellar-sdk";
 
+export function decodeEventValue(value: unknown) {
+  try {
+    return scValToNative(value as any);
+  } catch (err) {
+    return null;
+  }
+}
 export async function fetchEventsForRange(startLedger: number, contractIds: string[]) {
   return withRetry(() =>
     server.getEvents({
